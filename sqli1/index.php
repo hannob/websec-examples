f1b821a53e04e5efd79cbe2f821b19dcbb684134
Hanno Böck <hanno@gentoo.org>
2020-04-19 09:22:51 +0200

    sqli

---
+++ sqli1/index.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once("config.inc.php");
+
+if (!array_key_exists('user', $_POST)) {
+    ?>
+<form method="POST">
+User:<input type="text" name="user" value="admin"><br>
+Pass:<input type="text" name="pass" value=""><br>
+<input type=submit>
+</form>
+<?php
+
+    die();
+}
+
+$mi = new mysqli("localhost", $db_user, $db_pass, $db_name);
+if ($mi->connect_errno) {
+    die("Failed to connect to MySQL\n");
+}
+
+$query="SELECT * FROM users WHERE `user`='".$_POST['user'].
+"' AND `pass`='".$_POST['pass']."';";
+
+if (!$mi->multi_query($query)) {
+    if ($mi->errno) {
+        die($mi->error);
+    }
+    die("error with query");
+}
+$r = $mi->store_result();
+
+if ($r->num_rows >= 1) {
+    echo "login successful<br>\n";
+} else {
+    echo "login failed<br>\n";
+}
